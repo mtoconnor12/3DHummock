974a219f428047cbd8c3b70693eaa89ba642a0ab
Michael T Oconnor <mo8557@seismic15.geo.utexas.edu>
2018-10-09 12:32:18 -0400

    added first hillslope mesh

---
+++ mesh/hillslopemeshes/hillslope_mean_ac_mean_ct.py
@@ -0,0 +1,99 @@
+import sys, os
+
+sys.path.append(os.path.join(os.environ['ATS_SRC_DIR'],'tools','meshing_ats'))
+import meshing_ats
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import math
+
+# 2D Hillslope from a fucntion
+# Topography from a function: e.g., z = sine(x)
+
+x = np.linspace(0,500,501)
+x0,z0 = 0, 5
+m = 0.1
+Zsurf = z0 + 0.1 * (x - x0)
+
+m2 = meshing_ats.Mesh2D.from_Transect(x,Zsurf)
+
+# layer extrusion
+layer_types = []
+layer_data = []
+layer_ncells = []
+layer_mat_ids = []
+
+z=0
+Z = []
+n1 = 10 # ac thickness = 4 cm (1cm-thick layers * 2 layers) 
+n2 = 7 # ct thickness = 0 cm (2cm-thick layers * 0 layers)
+n3 = 20 # min thickness upper nodes
+n4 = 10 # min thickness lower nodes
+n5 = 12 # bedrock thickness
+
+for i in range(n1): # acrotelm
+    layer_types.append('constant')
+    layer_data.append(0.01)
+    layer_ncells.append(1)
+    layer_mat_ids.append(1001)
+    z = z + 0.01
+    Z.append(z)
+print (z)  
+
+for i in range(n2): # catotelm
+    layer_types.append('constant')
+    layer_data.append(0.02)
+    layer_ncells.append(1)
+    layer_mat_ids.append(1002)
+    z = z + 0.02
+    Z.append(z)
+print (z)
+
+dz = .02
+for i in range(n3): # mineral loess
+    dz *= 1.1
+    layer_types.append("constant")
+    layer_data.append(dz)
+    layer_ncells.append(1)
+    layer_mat_ids.append(1003)
+    #print ('3rd layer',z)
+    z = z + dz
+    Z.append(z)
+print (z)
+
+for i in range(n4): # mineral loess
+    dz *= 1.2
+    layer_types.append("constant")
+    layer_data.append(dz)
+    layer_ncells.append(1)
+    layer_mat_ids.append(1003)
+    #print ('3rd layer',z)
+    z = z + dz
+    Z.append(z)
+print (z)
+
+for i in range(n5): # bedrock
+    dz *= 1.2
+    layer_types.append("constant")
+    layer_data.append(dz)
+    layer_ncells.append(1)
+    layer_mat_ids.append(1004)
+    #print ('5th layer',z)
+    z = z + dz
+    Z.append(z)
+	
+print (z)
+print(len(layer_types),layer_types[-1])
+print(len(layer_mat_ids),layer_mat_ids[-1])
+
+#layer_types.append('snapped')
+#layer_data.append(-40.0) # bottom location
+#layer_ncells.append(2)
+#layer_mat_ids.append(1004)
+
+m3 = meshing_ats.Mesh3D.extruded_Mesh2D(m2, layer_types, 
+                                        layer_data, 
+                                        layer_ncells, 
+                                        layer_mat_ids)
+
+m3.write_exodus("hillslope-09Oct18_MeanAc_MeanCt.exo")
