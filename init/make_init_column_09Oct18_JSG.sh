28a5e1cc29295b268f5158e265c1273f2cfaa63a
Michael T Oconnor <mo8557@seismic15.geo.utexas.edu>
2018-10-10 14:23:50 -0400

    edited test7

--- init/make_init_column_09Oct18_JSG.sh
+++ init/make_init_column_09Oct18_JSG.sh
@@ -2,11 +2,7 @@
 #!/bin/sh
 # This script creates the 1D column file that gets fed to test7
 cur_dir=`pwd`
-cd $ATS_SRC_DIR/tools/utils/
-rm -rf *.h5
-cp $HOME/ats86/testing/3DHummock/test1/run02_09Oct18/visdump_data.h5 .
-cp $HOME/ats86/testing/3DHummock/test1/run02_09Oct18/visdump_mesh.h5 .
-python column_data.py -t 0
-cd $cur_dir
-cp $ATS_SRC_DIR/tools/utils/column_data.h5 column_data_run02_09Oct18.h5
+cd $HOME/ats86/testing/3DHummock/test1/run02_09Oct18/
+python $ATS_SRC_DIR/tools/utils/column_data.py -t 0
+mv column_data.h5 column_data_run02_09Oct18.h5
 echo 'Exp1 Column h5 file successfully created'
