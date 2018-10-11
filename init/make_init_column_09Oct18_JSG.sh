f63fa5619f6362cd4283d603af5982536ce38382
Michael O'Connor <mtoconnor12@gmail.com>
2018-10-11 14:08:42 -0400

    added changes to test7 input file

--- init/make_init_column_09Oct18_JSG.sh
+++ init/make_init_column_09Oct18_JSG.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-#!/bin/sh
 # This script creates the 1D column file that gets fed to test7
 cur_dir=`pwd`
-cd $HOME/ats86/testing/3DHummock/test1/run02_09Oct18/
+cd "../test1/run02_09Oct18/"
 python $ATS_SRC_DIR/tools/utils/column_data.py -t 0
-mv column_data.h5 column_data_run02_09Oct18.h5
+mv column_data.h5 ../../init/column_data_run02_09Oct18.h5
 echo 'Exp1 Column h5 file successfully created'
