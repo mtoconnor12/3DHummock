5db57a14da75c328c39d0e0dedcd11ae5d9881c5
Michael O'Connor <mtoconnor12@gmail.com>
2018-10-19 12:01:41 -0400

    built working python script to plot WT across different sims

--- test7/Plot_Active_Layer_Across_Sims.py
+++ test7/Plot_Active_Layer_Across_Sims.py
@@ -15,63 +15,72 @@ dt
 dt.strftime('%Y%m%d')
 
 num_runs = 18
-for j in range(num_runs)
+
+directory = "./run.1"
+keys, times, dat = parse_ats.readATS(directory, "visdump_data.h5", timeunits='yr')
+ind = 270
+col_dat = transect_data.transect_data(['saturation_ice'], keys=np.s_[ind], directory=directory)
+nvar, nt, nx, nz = col_dat.shape
+
+wtd = np.nan * np.ones((num_runs, nx),'d')
+
+for j in range(1,num_runs):
 	directory = "./run."+str(j)
 	keys, times, dat = parse_ats.readATS(directory, "visdump_data.h5", timeunits='yr')
 
 	## SELECT TIME INTERVAL TO PULL AND PLOT:
 	# Visdumps are spit out every 30 days.  Index 0 is January of year 1; 12 is January year 2; etc...
-	ind_start = 360
-	ind_end = 360*2 # if you want it to run to the end of the list, type '-1' for the last item on the list.
-	
-	ind_int = 30 # there is one checkpoint file every 30 days
+	ind = 270
 
 	col_dat = transect_data.transect_data(['saturation_ice'], keys=np.s_[ind], directory=directory)
 	times_subset = times[ind]
 
-	print times_subset
+	#print times_subset
 
 	nvar, nt, nx, nz = col_dat.shape
-	print nt
+	#print nt
 
 	# z_surf and z_bott is extrapolated based on dz, and is not necessarily exact if 
 	# dz varies in the top (respectively bottom) two cells
 	z_surf = col_dat[1,0,:,-1] + (col_dat[1,0,:,-1] - col_dat[1,0,:,-2])/2. # average of the uppermost and second-to-uppermost rows, shifted up to the top row
 	z_bott = col_dat[1,0,:,0] - (col_dat[1,0,:,1] - col_dat[1,0,:,0])/2. # average of the bottom and second-to-bottom rows, shifted up to the bottom row
 
-	print((col_dat[1,0,:,-1] - col_dat[1,0,:,-2])/2.)
+	#print((col_dat[1,0,:,-1] - col_dat[1,0,:,-2])/2.)
 
 	# We want the index of the deepest cell that has no ice.
 
 	# Determine the index of the lowest unsaturated cell at each time and x coordinate.
 	# Note that this is the right choice for water table, as a saturated zone might exist 
 	# on TOP of an unsaturated zone, for a perched aquifer, so we DON'T want the highest 
-	# saturated cell.
-	wtd = np.nan * np.ones((col_dat.shape[1], col_dat.shape[2]),'d')
+	# saturated cell
 
 	# print(col_dat[1,1,:,-1])
-	print(wtd.shape)
+	#print(wtd.shape)
 	for i in range(nx): # search each column across the domain
-		for k in range(nt): # search each time
-			where_unsat = np.where(col_dat[2,k,i,:] == 0)[0]
-			if len(where_unsat) == 0:
-				# nothing is unsaturated
-				wtd[k,i] = 0.
-			elif where_unsat[0] == 0:
-				# nothing is saturated, wtd is the full column thickness (i.e. the bottom)
-				wtd[k,i] = z_surf[i] - z_bott[i]
-			else:
-				# wt is in the domain -- average the first unsaturated with the last 
-				# saturated, then subtract from the surf to get depth
-				wtd[k,i] = z_surf[i] - (col_dat[1,k,i,where_unsat[0]] + col_dat[1,k,i,where_unsat[0]-1])/2 
-			print(where_unsat)
+		where_unsat = np.where(col_dat[2,0,i,:] == 0)[0]
+		if len(where_unsat) == 0:
+			# nothing is unsaturated
+			wtd[j,i] = 0.
+		elif where_unsat[0] == 0:
+			# nothing is saturated, wtd is the full column thickness (i.e. the bottom)
+			wtd[j,i] = z_surf[i] - z_bott[i]
+		else:
+			# wt is in the domain -- average the first unsaturated with the last 
+			# saturated, then subtract from the surf to get depth
+			#print i
+			#print z_surf[i]
+			#print col_dat.shape	
+			#print where_unsat.shape
+			#print col_dat[1,:,i,where_unsat[0]-1]
+			wtd[j,i] = z_surf[i] - (col_dat[1,:,i,where_unsat[0]] + col_dat[1,:,i,where_unsat[0]-1])/2 
+		#print(where_unsat)
 		
-	# plot
-	fig,ax = plt.subplots(1,1, figsize=(18,6))
-	cm = colors.cm_mapper(0,nt-1)
-	for k in range(nt):
-		ax.plot(col_dat[0,0,:,0], -wtd[k,:], color=cm(k), label="{0} years".format(times_subset[k]))
-	ax.set_xlabel("x [m]")
-	ax.set_ylabel("depth to ice table [m]")
-	ax.legend()
-	plt.show()
+# plot
+fig,ax = plt.subplots(1,1, figsize=(18,6))
+cm = colors.cm_mapper(0,num_runs-1)
+for j in range(1,num_runs):
+	ax.plot(col_dat[0,0,:,0], -wtd[j,:], color=cm(j), label="{0} run".format(j))
+ax.set_xlabel("x [m]")
+ax.set_ylabel("depth to ice table [m]")
+ax.legend()
+plt.show()
