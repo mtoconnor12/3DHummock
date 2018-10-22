ab7929f42a4cb6bf4f137bab898cd2c5eb2d2e95
Michael T Oconnor <mo8557@seismic15.geo.utexas.edu>
2018-10-22 12:12:21 -0400

    added changename sh file

---
+++ test7/RenameLastCheckpointFile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+NUMBERS=$(seq 1 18)
+echo ${NUMBERS}
+for NUM in ${NUMBERS}
+
+do
+	cd run.${NUM}
+	lastCp=$(ls -t checkpoint* | head -1)
+	cp $lastCp checkpoint_last.h5
+	cd ../
+done
