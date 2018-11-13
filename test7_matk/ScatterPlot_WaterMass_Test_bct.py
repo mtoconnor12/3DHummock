18172cc501ca80fc9f4c6e95760f8e7042b51495
Michael T Oconnor <mo8557@seismic15.geo.utexas.edu>
2018-11-13 14:41:21 -0500

    added scatter plot tools

---
+++ test7_matk/ScatterPlot_WaterMass_Test_bct.py
@@ -0,0 +1,65 @@
+#!usr/bin/env python
+"""Plots any dat file assuming time is in seconds.
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+import math
+
+nruns = 18
+c = 0
+leg = [None]*nruns
+m = [0.01,0.1]
+bac = [0.01,0.1,0.22]
+bct = [0.02,0.14,0.4]
+
+for i in range(2):
+	for j in range(3):
+		for k in range(3):
+			print m[i]
+			leg[c] = ['m=' + str(m[i]) + ' bac=' + str(bac[j]) + ' bct=' + str(bct[k])]
+			c = c + 1
+
+		
+
+leg_ordered = [None]*18
+
+# Low-sloping domains: simulations 1-9
+# Hi-sloping domains: simulations 10-18
+
+fix,axarr = plt.subplots(2,3,sharey=True,sharex=True)
+c = 1
+t1 = 10
+t2 = 20
+for i in range(2):
+	for j in range(3):	
+		t=np.empty(t2-t1)
+		y=np.empty(t2-t1)
+		mat = np.loadtxt('run_fromCheckpoint_yr10Debug_EvapFix.' + str(c) + '/subsurface_water_content.dat')
+		for k in range(t1,t2):
+			t[k-t1] = mat[365*k,0]/86400/365
+			y[k-t1] = mat[365*k,1]
+		axarr[i,j].plot(t,y)
+		t=np.empty(t2-t1)
+		y=np.empty(t2-t1)
+		mat = np.loadtxt('run_fromCheckpoint_yr10Debug_EvapFix.' + str(c+1) + '/subsurface_water_content.dat')
+		for k in range(t1,t2):
+			t[k-t1] = mat[365*k,0]/86400/365
+			y[k-t1] = mat[365*k,1]
+		axarr[i,j].plot(t,y)
+		t=np.empty(t2-t1)
+		y=np.empty(t2-t1)
+		mat = np.loadtxt('run_fromCheckpoint_yr10Debug_EvapFix.' + str(c+2) + '/subsurface_water_content.dat')
+		for k in range(t1,t2):
+			t[k-t1] = mat[365*k,0]/86400/365
+			y[k-t1] = mat[365*k,1]
+		axarr[i,j].plot(t,y)
+		axarr[i,j].legend([str(c) + '(min)',str(c+1) + '(mean)',str(c+2) + '(max)'])
+		axarr[i,j].set_title(str(int(m[i]*100)) + '% slope, ' + str(int(bac[j]*100)) + 'cm bAC')
+		axarr[i,j].set_ylabel('Mass [m3/time]')
+		axarr[i,j].set_xlabel('Time [yrs of sim]')
+		c = c + 3
+		plt.xlim(t1,t2)
+		#plt.xlabel('Time [yrs]')
+		#plt.title('subsuurface outlet flux')
+plt.show() 
---
+++ test7_matk/ScatterPlot_WaterMass_Test_bct_Across_Sims.py
@@ -0,0 +1,69 @@
+
+#!usr/bin/env python
+"""Plots any dat file assuming time is in seconds.
+"""
+import sys,os
+sys.path.append(os.path.join(os.environ['ATS_SRC_DIR'],'tools', 'utils'))
+import parse_xmf, parse_ats
+import column_data, transect_data
+import colors
+import mesh
+import numpy as np
+from datetime import datetime
+from matplotlib import pyplot as plt
+from matplotlib import colorbar
+
+
+
+nruns = 18
+c = 0
+leg = [None]*nruns
+m = [0.01,0.1]
+bac = [0.01,0.1,0.22]
+bct = [0.02,0.14,0.4]
+
+for i in range(2):
+	for j in range(3):
+		for k in range(3):
+			print m[i]
+			leg[c] = ['m=' + str(m[i]) + ' bac=' + str(bac[j]) + ' bct=' + str(bct[k])]
+			c = c + 1
+
+		
+
+leg_ordered = [None]*18
+
+# Low-sloping domains: simulations 1-9
+# Hi-sloping domains: simulations 10-18
+
+ind = 12*9 + 9 # time index from which we will extract thaw depths
+xPos = 15 # the horizontal position from which we will extract thaw depths
+
+# ONLY plot the SHALLOW SLOPING thaws (slope does not matter)
+
+wtd = np.nan*np.ones((np.size(bac),np.size(bct)),'d')
+for i in range(1):
+	for j in range(np.size(bac)):
+		for k in range(np.size(bct)):
+			directory = "./run_fromCheckpoint_yr10Debug_EvapFix."+str((j*3) + k + 1)
+			print directory
+			keys, times, dat = parse_ats.readATS(directory, "visdump_data.h5", timeunits='yr')
+			col_dat = transect_data.transect_data(['saturation_ice'], keys=np.s_[ind], directory=directory)
+			times_subset = times[ind]
+			nvar, nt, nx, nz = col_dat.shape
+			z_surf = col_dat[1,0,:,-1] + (col_dat[1,0,:,-1] - col_dat[1,0,:,-2])/2. # average of the uppermost and second-to-uppermost rows, shifted up to the top row
+			z_bott = col_dat[1,0,:,0] - (col_dat[1,0,:,1] - col_dat[1,0,:,0])/2. # average of the bottom and second-to-bottom rows, shifted up to the bottom row
+
+			where_unsat = np.where(col_dat[2,0,xPos,:] == 0)[0]
+			if len(where_unsat) == 0:
+				wtd[j,k] = 0.
+			elif where_unsat[0] == 0:
+				wtd[j,k] = z_surf[xPos] - z_bott[xPos]
+			else:
+				wtd[j,k] = z_surf[xPos] - (col_dat[1,:,xPos,where_unsat[0]] + col_dat[1,:,xPos,where_unsat[0]-1])/2 
+		plt.plot(bct,-wtd[j,:])
+plt.legend(['Min Acrotelm','Mean Acrotelm','Max Acrotelm'],loc='lower right')
+plt.title('Active Layer Thickness vs. Catotelm Thickness, Grouped by Acrotelm Thickness')
+plt.xlabel('Catotelm Thickness [m]')
+plt.ylabel('Active Layer Thickness [m]')
+plt.show() 
