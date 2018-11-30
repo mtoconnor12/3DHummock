967cd9d76e701f874cd3d85d25eda4ba8f37c990
Michael O'Connor <mtoconnor12@gmail.com>
2018-11-30 10:05:19 -0500

    after a 29Nov run

---
+++ test7_matk/CenturySim_29Nov18_Readme.txt
@@ -0,0 +1,5 @@
+DESCRIPTION OF SCENARIOS:
+1) CenturySim: 
+2) CenturySim_Debug_27Nov18: Checkpointed from CenturySim, with a 5cm dessicated zone.
+2) CenturySim_Debug_28Nov18: Checkpointed from CenturySim, with a 5cm dessicated zone and the rain/snow event on day 13782 edited to make it all rain. 
+3) CenturySim_29Nov18: Starting from SPINUP DAY 1, with a 6.5cm dessicated zone and the rain/snow event on day 13782 edited to make it all rain
---
+++ test7_matk/CenturySim_29Nov18_matk_ats.py
@@ -0,0 +1,74 @@
+import matplotlib
+matplotlib.use('agg')
+
+import numpy as np
+from matk import matk
+import parse_ats
+import atsxml
+
+runName = "CenturySim"
+suffix = "_29Nov18"
+#checkpointRunName = "_27Nov18"
+
+def model(pars, hostname='dum', processor=1):
+	# ATS ##############################################################################
+    	# Modify base ats xml input file and run ats
+	branchName = "hillslope-30mSuite"
+	fname = branchName + "-" + str(pars['slope']) + "m_" + str(pars['bac']) + "bac_" + str(pars['bct']) + "bct"
+	
+	m = atsxml.get_root('../test7_' + branchName + '_template_' + runName + suffix + '_input.xml')
+	
+	atsxml.replace_by_path(m,['mesh','domain','read mesh file parameters','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','computational domain acrotelm','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','computational domain catotelm','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','computational domain mineral','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','computational domain bedrock','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','surface','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	atsxml.replace_by_path(m,['regions','bottom face','region: labeled set','file'],'../../mesh/' + branchName + '/' + fname + '.exo')
+	
+    	atsxml.run(m, nproc=1, mpiexec='mpirun', stdout='stdout.out', stderr='stdout.err', cpuset=processor)
+	return True
+
+
+# Create cpusets, 4 cpus to a set
+# This is necessary so that the ATS runs get spread evenly over processors and doesn't stack up on processors
+# On our servers, the host key ('dum' below) isn't necessary since we run all jobs on the same server.
+# On clusters, you may be sending different runs to different hosts (computers). 
+# In that case, the dictionary keys are important for indicating the host.
+# The dictionary values (lists of integers) identify which processors to put each ATS run.
+njobs = 18
+nparams = 3
+hosts = {'dum': map(str, range(njobs))}
+
+# Instantiate MATK object specifying the "model" function defined above as the MATK "model"
+p = matk(model=model)
+
+# Add parameters that you want to sample over and their ranges
+p.add_par('slope',min=0.01, max=0.1, value=0.05)
+p.add_par('bac', min=0.01, max=0.22, value=0.1)
+p.add_par('bct',min=0.02, max=0.4, value=0.14)
+
+# Create matrix of parameter combinations
+m = [0.01,0.1]
+ac = [0.01,0.1,0.22]
+ct = [0.02,0.14,0.4]
+
+d = np.empty([njobs,nparams])
+c = 0
+for i in range(2):
+	for j in range(3):
+		for k in range(3):
+			d[c] = [m[i],ac[j],ct[k]]
+			c = c + 1			
+
+#d = [[0.01,0.01,0.02],[0.01,0.01,0.14],[0.01,0.01,0.4],[0.01,0.1,0.02],[0.01,0.1,0.14],[0.01,0.1,0.4],[0.01,0.
+
+# Create MATK sampleset
+s = p.create_sampleset(d)
+
+# Create parameter study of all combinations of min and max values for each parameter
+# s = p.parstudy(nvals=[2,3,3])
+# Save samples to file for inspection
+# s.savetxt('sample.txt')
+# Run sampleset using "hosts" dictionary defined above. 
+s.run(cpus=hosts, workdir_base=runName + suffix, reuse_dirs=True)
