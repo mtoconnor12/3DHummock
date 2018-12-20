996d24326e334281061d1fbc28eb0bc9e8636fa6
Michael O'Connor <mtoconnor12@gmail.com>
2018-12-20 16:09:44 -0500

    made a text file writer

--- test7_fieldParams_18Dec18/AllCols_Read_Last_Cycle_Time.sh
+++ test7_fieldParams_18Dec18/AllCols_Read_Last_Cycle_Time.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-fname="LastTimestep_$(date +"%Y_%m%d_%H%M").csv"
-touch $fname
+fname="AllColumns_$(date +"%Y_%m%d_%H%M").txt"
 suffix="_18Dec18"
+touch $fname
+
 echo "Filename: $fname"
 
-for colNames in TussockTundraHi TussockTundraLo WaterTrack WoodyShrubsHillslope SedgeHi WoodyShrubsRiparianHi SedgeLo FrostBoils
+for runName in TussockTundraHi TussockTundraLo WaterTrack WoodyShrubsHillslope SedgeHi WoodyShrubsRiparianHi SedgeLo FrostBoils
 do
-        runName=$colNames
 	NUMBERS=$(seq 1 32)
 	dot='.'
 	echo $runName
-	echo $runName >> $fname
+	# echo $runName >> $fname
 	for NUM in $NUMBERS
 	do
 		cd $runName$suffix$dot$NUM
-		echo 'Run #'$NUM >> ../$fname
-		tail -n 15000 stdout.out | grep Cycle | tail -1  >> ../$fname
+		#echo 'Run #'$NUM >> ../$fname
+		stuff=$(tail -n 15000 stdout.out | grep Cycle | tail -1)
+		echo -e "$runName\t$NUM\t$stuff" >> ../$fname
+		#sed -e "$NUM s/^/"$runName$underscore$NUM"/" -i ../$fname
 		cd ../
 	done
 done
